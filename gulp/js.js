c3b7eb0f343ebd48d8b2b33c61c76137180ab9c3
Kylik Dmitriy <BkForest20@gmail.com>
2019-02-04 17:02:37 +0200

    Added webpack stream to bundle js files

--- gulp/js.js
+++ gulp/js.js
@@ -13,12 +13,35 @@ const config = require('./config');
 
 gulp.task('js', function() {
     return gulp.src(`${config.src.js}/app.js`)
-        .pipe(sourcemaps.init())
-        .pipe(include())
-        .on('error', config.errorHandler.onError('JS Error include file'))
-        .pipe(babel())
-        .on('error', config.errorHandler.onError('Error: <%= error.message %>'))
-        .pipe(concat('app.js'))
+        .pipe(webpack({
+            output: {
+                filename: 'app.min.js'
+            },
+            mode: 'development',
+            devtool: 'inline-source-map',
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: [/node_modules/, /gulp/],
+                        use: {
+                            loader: 'babel-loader',
+                            query: {
+                                presets: ['@babel/preset-env']
+                            }
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(through.obj(function (file, enc, cb) {
+            // Dont pipe through any source map files as it will be handled
+            // by gulp-sourcemaps
+            const isSourceMap = /\.map$/.test(file.path);
+            if (!isSourceMap) this.push(file);
+            cb();
+        }))
         .pipe(uglify())
         .on('error', config.errorHandler.onError({
             message: [
@@ -45,62 +68,3 @@ gulp.task('copy:jsLib', function() {
 gulp.task('js:watch', function() {
     gulp.watch(`${config.src.js}/*.js`, ['js']);
 });
-
-// gulp.task('js', function() {
-//     return gulp.src(`${config.src.js}/*.js`)
-//         // .pipe(sourcemaps.init())
-//         .pipe(include())
-//         .on('error', config.errorHandler.onError('JS Error include file'))
-//         // .pipe(babel())
-//         // .on('error', config.errorHandler.onError('Error: <%= error.message %>'))
-//         .pipe(named())
-//         .pipe(webpack({
-//             // entry: {
-//             //     app: 'src/js/app.js'
-//             // },
-//             output: {
-//               filename: '[name].js'
-//             },
-//             devtool: 'source-map',
-//             module: {
-//               rules: [
-//                 {
-//                   test: /\.(js)$/,
-//                   exclude: /(node_modules)/,
-//                   loader: 'babel-loader',
-//                   query: {
-//                     presets: ['@babel/preset-env']
-//                   }
-//                 }
-//               ]
-//             }
-//             // resolve: {
-//             //     extensions: ['.js']
-//             // }
-//         }))
-//         .pipe(sourcemaps.init({loadMaps: true}))
-//         .pipe(through.obj(function (file, enc, cb) {
-//             // Dont pipe through any source map files as it will be handled
-//             // by gulp-sourcemaps
-//             const isSourceMap = /\.map$/.test(file.path);
-//             if (!isSourceMap) this.push(file);
-//             cb();
-//         }))
-//         .pipe(concat('app.js'))
-//         // .pipe(uglify())
-//         // .on('error', config.errorHandler.onError({
-//         //     message: [
-//         //         'Error: <%= error.message %>',
-//         //         '\n<%= error.cause %>',
-//         //         '\nLine: <%= error.cause.line %>',
-//         //         '\nPosition: <%= error.cause.pos %>',
-//         //         '\n<%= error.fileName %>'
-//         //     ]
-//         // }))
-//         // .pipe(rename('app.min.js'))
-//         // .pipe(gulp.dest(`${config.dest.js}/`))
-//         // .pipe(rename({suffix: '.min'}))
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest(`${config.dest.js}/`))
-//         .pipe(browserSync.stream({ once: true }));
-// });
