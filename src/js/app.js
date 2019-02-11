b6c2b85586c7097e864232e407ceab1035ea5a9e
Kylik Dmitriy <BkForest20@gmail.com>
2019-02-11 15:09:06 +0200

    Added viewing results feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,37 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+// export
+/* Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes
+*/
 
 const state = {};
 
 const controlSearch = async() => {
     // 1) get query from view
-    const query = 'pizza'; // TODO
+    const query = searchView.getInput(); // TODO
 
     // 2) add new Search object to state;
     state.search = new Search(query);
 
     // 3) Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
     // 4) get the results
     await state.search.getResults();
 
     // 5) Render results in UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
+    // 6) Clear input value
+
 };
 
 // Способ задавания ивентов. ( Не через document.querySelector);
-document.querySelector('.search').addEventListener('submit', function(event) {
+elements.searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
     controlSearch();
 });
---
+++ src/js/views/searchView.js
@@ -0,0 +1,28 @@
+import { elements } from './base';
+
+export const getInput = () => elements.searchInput.value;
+export const clearInput = () => {elements.searchInput.value = ''};
+export const clearResults = () => {elements.searchResList.innerHTML = ''};
+
+const renderRecipe = recipe => {
+    const markup = `
+    <li class="results_list_item">
+        <a class="results_link" href="#${recipe.recipe_id}">
+            <figure class="results_fig">
+                <img src=${recipe.image_url} alt="${recipe.title}">
+            </figure>
+            <div class="results_data">
+                <h4 class="results_name">${recipe.title}</h4>
+                <p class="results_author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+    `;
+    // Try to do this with documentFragment
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
+// func that recieves recipes array and prints them
+export const renderResults = recipes => {
+    console.log(recipes);
+    recipes.forEach(renderRecipe);
+};
