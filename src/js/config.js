6d40942c587b896f75760a1542a5e263b0bc26ae
Kylik Dmitriy <BkForest20@gmail.com>
2019-02-20 18:06:28 +0200

    Update config.js file

---
+++ src/js/config.js
@@ -0,0 +1,12 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const forkApi = {
+    key: '?key=2a95efc2bb6173de7201f31f9b18a2a9',
+    dataPath: 'https://www.food2fork.com/api/',
+    get: 'get',
+    search: 'search'
+};
+// https://spoonacular.com/food-api
+//  2a95efc2bb6173de7201f31f9b18a2a9
+// 34fd73875eea4b22c4d020f02e967e51
+// 212.90.191.41:30928
+// https://cors-anywhere.herokuapp.com/
