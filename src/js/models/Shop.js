a9c103f1892dfccdf79cc472127379a3375b6de9
Kylik Dmitriy <BkForest20@gmail.com>
2019-02-20 18:18:27 +0200

    Added shopModel functionality

---
+++ src/js/models/Shop.js
@@ -0,0 +1,30 @@
+import uniqid from 'uniqid';
+
+export default class List {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(count, unit, ingredient) {
+        const item = {
+            id: uniqid(),
+            count,
+            unit,
+            ingredient
+        };
+        this.items.push(item);
+        return item;
+    }
+
+    removeItem(id) {
+        // Get index of item
+        const itemIndex =  this.items.findIndex(elem => elem.id === id);
+        // Remove item
+        this.items.splice(itemIndex, 1);
+    }
+
+    updateCount(id, newCount) {
+        this.items.find(elem => elem.id === id).count = newCount;
+        // this.items[itemIndex].count = newCount;
+    }
+};
